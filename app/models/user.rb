9d0d70b13ff73e3b9208a222ae68895546eceb97
Jason Liu <jasobs10@gmail.com>
2018-04-24 15:37:28 -0400

    Add password authentication methods to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,58 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  f_name          :string
+#  l_name          :string
+#  motto           :string
+#  receive_emails  :boolean          default(FALSE)
+#  current_points  :integer          default(0)
+#  access_type     :string           default("user")
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 4, allow_nil: true }
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    # ruby's method missing to generate this method
+    user = User.find_by_username(username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+
 end
